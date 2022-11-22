05ee12088a46dff7574e79f4bcff0380e4d7fc7d
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-22 14:47:36 +0100

    site js prototype

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button#confirm');
+const button = document.getElementById('confirm');
 
 button.addEventListener('click', function(){
     let distance = parseFloat(document.getElementById('distance').value);
@@ -6,5 +6,16 @@ button.addEventListener('click', function(){
     let age = parseInt(document.getElementById('age').value);
     console.log(age);
     let price = distance * 0.2762;
-    console.log(price);
+
+    if (age < 18){
+        price = price * 0.825;
+        console.log(price.toFixed(2));
+    } 
+    else if (age > 65){
+        price = price * 0.667;
+        console.log(price.toFixed(2));
+    } else {
+        console.log(price.toFixed(2));
+    }
+
 });
