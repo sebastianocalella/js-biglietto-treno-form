aeb5f0d72f1a1205abdd9e623547d9bab9ae958b
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-22 15:38:00 +0100

    adding features

--- script.js
+++ script.js
@@ -10,14 +10,18 @@ button.addEventListener('click', function(){
     if (age < 18){
         price = price * 0.825;
         console.log(price.toFixed(2));
+        document.getElementById('answer').insertAdjacentHTML('beforebegin','<p><em>sei minorenne? </em>hai diritto ad uno sconto del 17,5%!</p>');
+        document.getElementById('answer').insertAdjacentHTML('afterEnd',`<p>hai risparmiato ben:  ${(price.toFixed(2)/0.825*0.175).toFixed(2)} &euro; </p>`);
     } 
     else if (age > 65){
         price = price * 0.667;
         console.log(price.toFixed(2));
+        document.getElementById('answer').insertAdjacentHTML('beforebegin','<p><em>hai più di 65 anni? </em>hai diritto ad uno sconto del 33.3%!</p>');
+        document.getElementById('answer').insertAdjacentHTML('afterEnd',`<p>hai risparmiato ben:  ${(price.toFixed(2)/0.667*0.333).toFixed(2)} &euro; </p>`);
     } else {
         console.log(price.toFixed(2));
     }
-
-    document.querySelector('p.price').innerHTML = `in base ai dati da te forniti il costo del biglietto è di: ${price.toFixed(2)} &euro;`;
+    
+    document.getElementById('answer').innerHTML = `in base ai dati da te forniti il costo del biglietto è di: ${price.toFixed(2)} &euro;`;
 
 });
